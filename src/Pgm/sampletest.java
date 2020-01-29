9656eb16dc664f084cd95e3e1e32714a4c34b6a9
Taslimtaj <tashuguddadamane@gmail.com>
2020-01-29 13:18:36 +0530

    first commited

---
+++ src/Pgm/sampletest.java
@@ -0,0 +1,16 @@
+package Pgm;
+
+import org.testng.Reporter;
+import org.testng.annotations.Test;
+
+public class sampletest
+{
+	
+	@Test
+	public void method()
+	{
+		Reporter.log("SAmple test created");
+		
+	}
+
+}
